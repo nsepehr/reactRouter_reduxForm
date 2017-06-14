e996f26cf18a1e9505d0134effcb9fe67c0a930f
Nima Sepehr <nima.sepehr@intel.com>
2017-06-13 22:27:26 -0700

    Update with new posts, getting all posts

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,16 +1,87 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
-  render() {
+  renderField(field) {
+    // Below is a compacted syntax in ES6 for destructuring to access properties
+    const { meta: { touched, error } } = field;
+    const css = `form-group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div>
-        Posts New !
+      <div className={css}>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          // This is the redux requirement
+          // It has all of the event handlers like onChange and etc.
+          {...field.input}
+        />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
+    )
+  }
+
+  onSubmit(values) {
+    console.log(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      // Equivalent to writing old javascript of:
+      // {handleSubmit(this.onSubmit.bind(this))}
+      <form onSubmit={handleSubmit((values) => this.onSubmit(values))}>
+        <Field
+          label="Title for Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories of Post"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary" >
+          Submit
+        </button>
+        <Link to="/" className="btn btn-danger"> Cancel </Link>
+      </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  // validate the inputs from 'values' object
+  for (let check of ['title', 'categories', 'content']) {
+    if (!values[check]) {
+      errors[check] = `Please enter a value for ${check}`;
+    }
+  }
+
+  // If erros is empty, the form is valid.
+  // however, if errors has any objects, then redux will think it's not valid and not submit it
+  return errors;
+}
+
 export default reduxForm({
+  validate, // same as validate: validate
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state={}, action) {
   switch (action.type) {
+    case FETCH_POST:
+    /* this is the old ES5 way of doing things
+      const post = action.payload.data;
+      const newState = { ...state };
+      newState[post.id] = post;
+      return newState;
+    */
+    // New ES6 format of the same code above
+    return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
       // Check this playground JS site
